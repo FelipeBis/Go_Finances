1a7083dfd7a31b9dd9b1815f7e95366eb82e8294
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2021-06-25 15:53:01 -0300

    version 1.0

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,48 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+
+        secondary: '#FF872C',
+        secondary_light: 'rgba(255, 135, 44, 0.3)',
+
+        sucess: '#12A454',
+        sucess_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_light: 'rgba(232,63, 91, 0.5)',
+
+        shape: '#FFFFFF',
+
+        title: '#363F5F',
+
+        text: '#969CB2',
+
+        text_dark: '#000000',
+
+        background: '#F0F2F5',
+    },
+    colors_dark: {
+        primary: '#3C2C79',
+
+        secondary: '#E66300',
+        secondary_light: 'rgba(230, 99, 0, 0.3)',
+
+        success: '#12A152',
+        success_light: 'rgba(18, 161, 82, 0.5)',
+
+        attention: '#A01116',
+        attention_light: 'rgba(160, 17, 22, 0.5)',
+
+
+        shape: '#272727',
+        title: '#FFFFFF',
+        text: '#F3F6FF',
+        text_dark: '#A6A6A6',
+        background: '#121212'
+    },
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',   
+    },
+};
